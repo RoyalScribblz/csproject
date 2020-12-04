9bffd011486c10448a438e21ffa12a8cb85cc475
Royal_Scribblz <clashmonster123@gmail.com>
2020-12-04 12:30:16 +0000

    fixed PEP8 on binance wrapper

--- binance_wrapper.py
+++ binance_wrapper.py
@@ -3,7 +3,7 @@ import requests
 
 def get_klines(symbol, interval):
     if interval.endswith(("m", "h", "d", "w", "M")):
-        return requests.get("https://api.binance.com/api/v3/klines?symbol=" + symbol.upper() + "&interval=" + interval) \
+        return requests.get("https://api.binance.com/api/v3/klines?symbol=" + symbol.upper() + "&interval=" + interval)\
             .json()
     else:
         raise Exception("Kline interval must be m, h, d, w, or M")
